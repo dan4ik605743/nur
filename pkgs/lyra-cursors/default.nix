52926865e1aa9542b985000ab549b163ac26bc0c
dan4ik <6057430gu@gmail.com>
2021-06-18 12:36:37 +0700

    add my files

---
+++ pkgs/lyra-cursors/default.nix
@@ -0,0 +1,20 @@
+{ stdenv
+, fetchFromGitHub
+}:
+
+stdenv.mkDerivation 
+{
+  name = "my-cursor";
+  src = fetchFromGitHub 
+  {
+    owner = "dan4ik605743";
+    repo = "lyra-cursors";
+    rev = "cd609236710ef6152592bb0b6348f7de30512309";
+    sha256 = "137l6kfl46x1p096f17hxpnxv74mji01bm156llfm5470vzmj5dc";
+  };
+  installPhase = 
+  ''
+    install -dm 755 $out/share/icons
+    cp -dr --no-preserve='ownership' Lyra{B,F,G,P,Q,X}-cursors $out/share/icons/
+  '';
+}
